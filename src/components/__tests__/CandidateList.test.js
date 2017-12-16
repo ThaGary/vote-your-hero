0035ebe6764b6ee697c995f0527dac6ea2967a08
zkan <kan@prontomarketing.com>
2017-12-16 21:42:08 +0700

    Create CandidateList :bear:

---
+++ src/components/__tests__/CandidateList.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme';
+import CandidateList from '../CandidateList'
+
+describe('<CandidateList />', () => {
+  it('renders list of candidates', () => {
+    const wrapper = shallow(<CandidateList candidates={['Kan', 'Mils']} />)
+    console.log(wrapper)
+    expect(wrapper.html()).toEqual('<ol><li>Kan</li><li>Mils</li></ol>')
+  })
+})
