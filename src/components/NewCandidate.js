bcb627613ecc94e48382a41937626c68b740853e
zkan <kan@prontomarketing.com>
2017-12-17 08:16:45 +0700

    Create NewCandidate stateless component :bear:

---
+++ src/components/NewCandidate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewCandidate = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default NewCandidate
