a39c8e8774f6be5b59477702c97e77c029059509
zkan <kan@prontomarketing.com>
2017-12-17 17:37:57 +0700

    Change CandidateList to stateful component and define propTypes :bear:

--- src/components/CandidateList.js
+++ src/components/CandidateList.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const CandidateList = ({ candidates }) => {
-  return (
-    <React.Fragment>
-      <h1>Candidate List</h1>
-      <ol>
-        {candidates.map((candidate, i) => <li key={i}>{candidate}</li>)}
-      </ol>
-    </React.Fragment>
-  )
+class CandidateList extends React.Component {
+  static propTypes = {
+    candidates: PropTypes.array,
+  }
+
+  render() {
+    const { candidates } = this.props
+
+    return (
+      <React.Fragment>
+        <h1>Candidate List</h1>
+        <ol>
+          {candidates.map((candidate, i) => <li key={i}>{candidate}</li>)}
+        </ol>
+      </React.Fragment>
+    )
+  }
 }
 
 export default CandidateList
