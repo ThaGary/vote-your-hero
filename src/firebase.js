c1ef0908e2c87fa63ecf9107d5eba8f4d47f010d
zkan <kan@prontomarketing.com>
2018-01-03 22:10:25 +0700

    Add firebase api key

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: 'AIzaSyBtNm5setCj1czuQFgBCEDLzF9xI-mFXU4',
+  authDomain: 'vote-your-hero.firebaseapp.com',
+  databaseURL: 'https://vote-your-hero.firebaseio.com',
+  projectId: 'vote-your-hero',
+  storageBucket: 'vote-your-hero.appspot.com',
+  messagingSenderId: '652756463987'
 }
 
 firebase.initializeApp(config)
