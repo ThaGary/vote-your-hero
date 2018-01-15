17bd8a89d9fc7259baf7a6418f3e3a93e04e4476
zkan <kan@prontomarketing.com>
2018-01-16 06:46:18 +0700

    Calculate total votes and show :bear:

--- src/components/Result.js
+++ src/components/Result.js
@@ -7,27 +7,32 @@ class Result extends Component {
   state = {
     data: [],
     loading: true,
+    total: 0,
   }
 
   componentDidMount() {
     firebase.database().ref('items').on('value', (data) => {
       const items = data.val();
       let candidates = []
+      let total = 0
       for (let key in items) {
         let item = items[key]
         candidates.push({
           text: item.name,
           value: item.count,
         })
+        total += item.count
       }
       this.setState({
         data: candidates,
-        loading: false
+        loading: false,
+        total,
       })
     })
   }
 
   render() {
+    const { data, total } = this.state
     const width = 700
     const height = 400
     const margin = {left: 100, right: 100, top: 50, bottom: 50}
@@ -36,11 +41,12 @@ class Result extends Component {
       <Container>
         <Segment>
           <Header as='h1'>The Winner is...</Header>
+          <p><strong>Total:</strong> {total}</p>
           <BarChart
             width={width}
             height={height}
             margin={margin}
-            data={this.state.data}
+            data={data}
           />
         </Segment>
       </Container>
